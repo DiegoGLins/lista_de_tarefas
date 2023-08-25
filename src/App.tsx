bb58d7d2437c74419d0dda471c37b97c4f1d8b61
Diego Lins <you@example.com>
2023-08-25 10:55:02 -0300

    Initial commit
    Co-authored-by: Matheus Palma <MatheusFPalma@users.noreply.github.com>Refatoração e inclusão de funcionalidades

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,107 @@
-import { useState } from 'react'
+
 import * as Component from './App.styles'
-import { v4 as createUuid } from "uuid";
-import { Item } from './types/Item'
-import ListItem from './components/ListItem/index'
 import { Area } from './components/Area'
-
-const App = () => {
-
-  const id = createUuid()
-  const [list, setList] = useState<Item[]>([])
-
-
-  const handleAddTask = (taskName: string) => {
-    const newList = [...list]
-    newList.push({
-      id: id,
-      name: taskName,
-      done: false
-    })
-    setList(newList)
-  }
+import ButtonDefault from './components/Button/ButtonDefault';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { IconButton } from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
 
 
+import React, { useState, useEffect, useRef, useCallback, useMemo } from 'react';
+import { ItemType } from './types/ItemType';
+import { ItemStyled } from './components/ListItem/styles';
+
+
+const App: React.FC = () => {
+  const [tasks, setTasks] = useState<ItemType[]>([]);
+  const [filter, setFilter] = useState<string>('Todas');
+  const [inputText, setInputText] = useState<string>('')
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const addTask = useCallback((name: string) => {
+    const newTask: ItemType = {
+      id: tasks.length + 1,
+      name,
+      done: false,
+    };
+    setInputText('')
+    setTasks(prevTasks => [...prevTasks, newTask]);
+  }, [tasks]);
+
+  const toggleTask = useCallback((taskId: number) => {
+    setTasks(prevTasks =>
+      prevTasks.map(task =>
+        task.id === taskId ? { ...task, done: !task.done } : task
+      )
+    );
+  }, []);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      setInputText(inputText)
+      console.log('Input value:', inputRef.current.value);
+    }
+  }, [inputText]);
+
+  const filteredTasks = useMemo(() => {
+    if (filter === 'done') {
+      return tasks.filter(task => task.done);
+    } else if (filter === 'Pendentes') {
+      return tasks.filter(task => !task.done);
+    } else {
+      return tasks;
+    }
+  }, [tasks, filter]);
 
   return (
+
     <Component.Container>
       <Component.Area>
         <Component.Header>
           Lista de Tarefas
         </Component.Header>
-        <Area onEnter={handleAddTask} />
-        {list.map((item, index) => (
-          <ListItem key={index} item={item} />
+        <Area>
+          <input type="text"
+            placeholder='Adicione uma tarefa'
+            value={inputText}
+            onChange={e => setInputText(e.target.value)}
+          />
+          <button onClick={() => addTask(inputText)}>Adicionar Tarefa</button>
+        </Area>
+
+        <div style={{ display: 'flex', justifyContent: 'center', gap: '15px' }}>
+          <ButtonDefault actionConfirm={() => setFilter('Todas')} title={'Todas'} />
+          <ButtonDefault actionConfirm={() => setFilter('Pendentes')} title={'Pendentes'} />
+          <ButtonDefault actionConfirm={() => setFilter('done')} title={'Concluídas'} />
+        </div>
+
+        {filteredTasks.map(task => (
+          <div key={task.id}>
+            <ItemStyled done={task.done}>
+              <input
+                type="checkbox"
+                checked={task.done}
+                onChange={() => toggleTask(task.id)}
+              />
+              <p style={{ color: '#ccc', textDecoration: task.done ? 'line-through' : 'initial' }}>
+                {task.name}
+              </p>
+              <div style={{ justifyContent: 'flex-end', paddingInlineStart: '20px' }}>
+                <IconButton style={{ color: '#25d60e' }} edge="end" aria-label="delete">
+                  <EditIcon />
+                </IconButton>
+                <IconButton style={{ color: '#e3f42a' }} edge="end" aria-label="delete">
+                  <DeleteIcon />
+                </IconButton>
+              </div>
+            </ItemStyled>
+          </div>
         ))}
 
       </Component.Area>
-    </Component.Container>
-  );
-}
+    </Component.Container >
 
-export default App
+  );
+};
+
+export default App;
