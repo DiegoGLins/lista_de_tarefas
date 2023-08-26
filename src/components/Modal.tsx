240a6f4bdd4ead6f1ce336420e92e4bd9ee464d9
Diego Lins <you@example.com>
2023-08-25 21:39:20 -0300

    Criação do Modal

---
+++ src/components/Modal.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '25px',
+    gap: '15px'
+
+};
+
+interface ModalProps {
+    isOpen: boolean;
+    OnClose?: boolean;
+    children: React.ReactNode;
+}
+
+export default function BasicModal({ isOpen, children }: ModalProps) {
+
+    return (
+        <div>
+            <Modal
+                open={isOpen}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    {children}
+                </Box>
+            </Modal>
+        </div>
+    );
+}
